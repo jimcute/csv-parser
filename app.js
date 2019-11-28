4c2e0a4724deb6d8db1cd7acca0760e8ae4ec120
Jameer <jimcute8879@gmail.com>
2019-11-29 01:30:51 +0530

    morgan and debug packages removed

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -29,7 +28,6 @@ var db = mongoose.connect('mongodb://localhost:27017/csvdb', {
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
